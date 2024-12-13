b6298097373840c94dfd58514b9b2b29f023806f
Lucas Felipe <outrolucas2002@gmail.com>
2024-12-13 18:08:52 -0300

    alternative timeline

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,14 @@
 import { getRandomPhrase } from "@/actions/phraseAction";
 import { redirect } from "next/navigation";
 
+export const dynamic = "force-dynamic";
+
 export default async function Home() {
   const phrase = await getRandomPhrase([]);
+
+  const redirectPage = (id: number) => {
+    window.location.href = `/${id}`;
+  };
 
   if (!phrase) {
     return <div>No phrases found</div>;
